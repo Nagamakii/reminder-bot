b3828f053f46c9e477273f725acb15ec62d6e7fc
Evan Quah <evanjquah@gmail.com>
2022-04-11 01:01:06 -0500

    uhhh idfk

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
+import asyncio
+from urllib import response
+from urllib.request import urlopen
+from aiohttp import request
 from discord.ext import commands
 from dotenv import load_dotenv
 import os
-from threading import Timer
+import psycopg2
+import websockets
 
 load_dotenv()
 
-bot = commands.Bot(command_prefix='!', description="This is a test bot")
-token = os.getenv("TOKEN") 
 
-@bot.command(name="remindme")
-async def reminder(ctx, arg):
-    await ctx.send(f"Gotcha! I will send you a reminder to {arg} when you get home.")
+bot = commands.Bot(command_prefix='!', description="This is a test bot")
+token = os.getenv("TOKEN")
+
+
+
+# Command that parses the reminders
+@bot.command()
+async def remindme(ctx, arg):
+    await ctx.send(f"I will remind you to {arg} when you get home!")
     global x
     x = arg
 
-@bot.command(name="home")
-async def user_home(ctx):
-    await ctx.send(f"Remember to {x}!")
-
+print('running websockets ws://192.168.4.21:8000')
+server = websockets.serve(response, '192.168.4.21', '8000')
+asyncio.get_event_loop().run_until_complete(server)
 
 if __name__ == '__main__':
     bot.run(token)
