43d61b6adf381cd13600cd2cce72a21a297cca51
Evan Quah <evanjquah@gmail.com>
2022-05-07 23:32:25 -0500

    Find out how to handle more than 1 arg in discord

--- main.py
+++ main.py
@@ -1,45 +1,40 @@
+from discord import Webhook, RequestsWebhookAdapter
+from flask import Flask, request, abort
 from discord.ext import commands
 from dotenv import load_dotenv
-import psycopg2
 import os
+import threading
 
 load_dotenv()
+app = Flask(__name__)
 
 bot = commands.Bot(command_prefix='!', description="This is a bot to remind me of things")
+wh = os.getenv('WEBHOOK')
 token = os.getenv("TOKEN")
 db_token = os.getenv("DB_PASS")
 
-conn = None
-# read connection parameters
-print('Connecting to the PostgreSQL database...')
-conn = psycopg2.connect(user="db_user",
-                          password=db_token,
-                          host="192.168.4.59",
-                          port="5433",
-                          database="postgres")
-cur = conn.cursor()
-        
 @bot.event
 async def on_ready():
     print("Bot is online!")
 
+reminders = []
 @bot.command()
 async def remindme(ctx, arg):
     await ctx.send(f"I will remind you to {arg} when you get home!")
-    insert = (cur.execute(f""" INSERT INTO COMMANDS (ID, CONTENT) VALUES (1, '{arg}') """))
-    conn.commit()
+    reminders.append(arg)
 
-@bot.command()
-async def home(ctx):
-    try:
-        select = (cur.execute(""" SELECT content FROM commands"""))
-        await ctx.send(f"Welcome home {ctx.author.mention}! Don't forget to {select}")
-        conn.commit()
-    finally:
-        rm = (cur.execute(""" TRUNCATE commands """))
-        (rm)
-        conn.commit()
-        conn.close()
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    print(request.method)
+    if request.method == 'POST':
+        print(request.json)
+        formatted_list = (', '.join(reminders))
+        Webhook.from_url(wh, adapter=RequestsWebhookAdapter()).send(f"Remember to {formatted_list} <@383762688355991554>!", username='Reminder-bot')
+        return 'Home!', 200
+    else:
+        abort(400)    
+
+threading.Thread(target=lambda: app.run(host="0.0.0.0")).start()
 
 if __name__ == '__main__':
     bot.run(token)
